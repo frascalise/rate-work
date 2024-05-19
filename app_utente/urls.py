43d072d54c2e2f94699154e697a1082ef8c19556
frascalise <scalise92111@gmail.com>
2024-05-19 18:20:42 +0200

    Aggiunta visualizzazione utenti con lista e singolarmente

--- app_utente/urls.py
+++ app_utente/urls.py
@@ -6,4 +6,5 @@ app_name = 'app_utente'
 
 urlpatterns = [
     path('<int:pk>/', views.UtenteDetail.as_view(), name='utente_detail'),  # es. http://127.0.0.1:8000/utente/2/
+    path('', views.UtenteList.as_view(), name='utente_list')
 ]
